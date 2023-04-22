761c30de35a8ae6ca91534d1e989d193e08d7a43
eniigma1337 <anuragn.0505@gmail.com>
2023-04-22 06:56:58 -0400

    reducing redundant imports

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,7 +3,6 @@ import Layout from "../components/Layout";
 import About from "../components/About";
 import Project from "../components/Project";
 import Contact from "../components/Contact";
-import Header from "../components/Header";
 
 export default function Home() {
   return (
