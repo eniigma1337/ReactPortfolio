2a246aacc29c362fdf2aa33846269c8cbe7b334a
eniigma1337 <anuragn.0505@gmail.com>
2023-04-22 06:57:18 -0400

    reducing redundant imports

--- src/routes/Experience.jsx
+++ src/routes/Experience.jsx
@@ -1,8 +1,5 @@
 import React from "react";
-import Layout from "../components/Layout";
-import About from "../components/About";
 import Contact from "../components/Contact";
-import Header from "../components/Header";
 import Education from "../components/Education";
 
 export default function Experience() {
